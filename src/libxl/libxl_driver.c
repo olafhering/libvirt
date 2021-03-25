e079bde3e7cb20841106716dbe7bae82ece3c186
Olaf Hering <olaf@aepfle.de>
2021-03-25 17:26:09 +0100

    06393ff7d31da344223274e27e8055de90c87082
    libxl: add API wrapper for libxl_set_vcpuonline
    
    Upcoming changes will use different LIBXL_API_VERSION variants.
    
    Prepare libxl_set_vcpuonline, which got a new parameter
    "ao_how" in Xen 4.12. libvirt does not use this parameter.
    
    No functional change intended.
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>
    Reviewed-by: Jim Fehlig <jfehlig@suse.com>

--- src/libxl/libxl_driver.c
+++ src/libxl/libxl_driver.c
@@ -2334,7 +2334,7 @@ libxlDomainSetVcpusFlags(virDomainPtr dom, unsigned int nvcpus,
         break;
 
     case VIR_DOMAIN_VCPU_LIVE:
-        if (libxl_set_vcpuonline(cfg->ctx, vm->def->id, &map) != 0) {
+        if (libxlSetVcpuonlineWrapper(cfg->ctx, vm->def->id, &map) != 0) {
             virReportError(VIR_ERR_INTERNAL_ERROR,
                            _("Failed to set vcpus for domain '%d'"
                              " with libxenlight"), vm->def->id);
@@ -2345,7 +2345,7 @@ libxlDomainSetVcpusFlags(virDomainPtr dom, unsigned int nvcpus,
         break;
 
     case VIR_DOMAIN_VCPU_LIVE | VIR_DOMAIN_VCPU_CONFIG:
-        if (libxl_set_vcpuonline(cfg->ctx, vm->def->id, &map) != 0) {
+        if (libxlSetVcpuonlineWrapper(cfg->ctx, vm->def->id, &map) != 0) {
             virReportError(VIR_ERR_INTERNAL_ERROR,
                            _("Failed to set vcpus for domain '%d'"
                              " with libxenlight"), vm->def->id);
